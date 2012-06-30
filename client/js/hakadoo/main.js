38984a3bcf5ae39f0a0a4967a646fc332538bdda
Kirk Kahn <kirk@kirkkahn.com>
2012-06-30 18:50:45 -0400

    added timer

--- client/js/hakadoo/main.js
+++ client/js/hakadoo/main.js
@@ -7,7 +7,28 @@ $(document).ready(function() {
         lineNumbers: true,
         matchBrackets: true
     });
-    
+
+    //pad timer with zeros
+    var lpad = function(value, padding) {
+        var zeroes = "0";
+
+        for (var i = 0; i < padding; i++) { zeroes += "0"; }
+
+        return (zeroes + value).slice(padding * -1);
+    }
+    var remaining, elapsed = 0, limit = 300; //amount of time in seconds
+    var timer = setInterval(function() {
+        elapsed++;
+        remaining = limit-elapsed;
+        $("#timer").html(function() { //display time
+            return Math.floor(remaining/60)+":"+lpad(remaining-(Math.floor(remaining/60)*60),2);
+        });
+        if (remaining == 0) { //timer finished
+            clearInterval(timer);
+            alert("timer finished");
+        }
+    }, 1000);
+
     // Disable Cut, Copy and Paste in the Code Mirror
     $(".CodeMirror*").live("cut copy paste", function(e) {
         e.preventDefault();
