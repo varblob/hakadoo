cbb7783f6f3ecf2efb55795863e65b3dae086422
tac.Tony <tony@traderanalyticscorp.com>
2012-06-30 15:32:15 -0400

    adding some basic event handling.  Turned on CodeMirror

--- client/js/hakadoo/remote.js
+++ client/js/hakadoo/remote.js
@@ -2,20 +2,36 @@ $.hakadoo = $.hakadoo || {};
 $.hakadoo.remote = function(){
 	'use strict';
 	// Connect to socket.io
-	var socket = io.connect(window.Array.host);
-	
-	$('#you').bind('change keyup', function() {
-		var text = $('#you').val();
+	var socket = io.connect(window.Array.host),
+		you = CodeMirror.fromTextArea(document.getElementById("user_code"), {
+			lineNumbers: true,
+			matchBrackets: true
+		}),
+		them = CodeMirror.fromTextArea(document.getElementById("compete_code"), {
+			lineNumbers: true,
+			matchBrackets: true
+		});
+	 
+	$('#user_cade').bind('change keyup', function() {
+		var text = you.getValue();
 		console.log('sending text', text);
-		socket.emit('textEntered', {text: text});
+		socket.emit('textUpdate', {text: text});
 	});
 	
 	socket.on('you:textUpdate', function(data) {
-		$('#opponent').text(data.text);
+		you.setValue(data.text);
 	});
 	  
 	socket.on('other:textUpdate', function(data) {
-		$('#opponent').text(data.text);
+		them.setValue(data.text);
 	});
+	
+	socket.on('waiting', function(data){
+		console.log('waiting');
+	})
+	
+	socket.on('ready', function(data){
+		console.log('ready');
+	})
 	
 };
