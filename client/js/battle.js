373e00be0d638d0b111e062a71a5e799d938af51
Brenden Kokoszka <bkokoszk@nd.edu>
2012-06-30 14:44:57 -0400

    pairing model

--- client/js/battle.js
+++ client/js/battle.js
@@ -1,15 +1,25 @@
 $(document).ready(function() {
 
   // Connect to socket.io
-  var socket = io.connect('http://localhost');
+  var socket = io.connect('http://' + window.location.host);
 
-  $('#you').bind('change keyup', function() {
-    var text = $('#you').val();
-    console.log('sending text', text);
-    socket.emit('textEntered', {text: text});
+  // Get either 'waiting' or 'ready'
+  socket.on('waiting', function() {
+    $('#status').text('Waiting...');
   });
 
-  socket.on('textUpdate', function(data) {
-    $('#opponent').text(data.text);
+  socket.on('ready', function() {
+    $('#status').text('Go!');   
+
+    // Bind the battle events
+    $('#you').bind('change keyup', function() {
+      var text = $('#you').val();
+      console.log('sending text', text);
+      socket.emit('textEntered', {text: text});
+    });
+
+    socket.on('textUpdate', function(data) {
+      $('#opponent').text(data.text);
+    });
   });
 });
--- server/sockets.js
+++ server/sockets.js
@@ -2,21 +2,50 @@ var socketIO = require('socket.io')
   , flatiron = require('flatiron')
   , app = flatiron.app;
 
-exports.connections = [];
+// The socket connection waiting for a partner
+exports.single = null;
 
 exports.startListening = function() {
 
   var io = socketIO.listen(app.server);
   console.log('Listening', app.server);
 
-  io.sockets.on('connection', function (socket) {
-  
-    
+  io.sockets.on('connection', function (socket) { 
+    console.log('connection from', socket);
 
-    socket.on('textEntered', function(data) {
-      var text = data.text.toUpperCase();
-      console.log('go text', text);
-      socket.emit('textUpdate', { text: text });
-    });
+    // A partner is already waiting
+    if (exports.single) {
+      var partner = exports.single;
+      var players = [socket, partner];
+      exports.single = null;
+  
+      // Battle logic...
+      bindBattleLogic(players);
+  
+    // Otherwise, this is an odd connection. Wait for a partner
+    } else {
+      exports.single = socket;
+      socket.emit('waiting');
+    }
   });
 };
+
+
+/*
+ * Binds the event for handling battle mechanics
+ * @param (Array) players
+ */ 
+function bindBattleLogic(players) {
+
+  players.forEach(function(me, i) {
+    var opponent = players[i^1];
+
+    me.emit('ready', {});
+
+    me.on('textEntered', function(data) {
+      var text = data.text.toUpperCase();
+      console.log('go text', text);
+      opponent.emit('textUpdate', { text: text });
+    });
+  });
+}
