a50d80d54836224dbc3660d9728ddbbde33ea31b
Lucas Lemanowicz <lemanowiczlucas@gmail.com>
2012-07-01 20:31:31 -0400

    Starting to implement the Question MongoDB model

--- client/js/lib/jquery.hakadoo.js
+++ client/js/lib/jquery.hakadoo.js
@@ -5,20 +5,20 @@ jQuery.hakadoo = ( function($) {
     var _questions = [{
 	      question: 'Write a function that sums the numbers 1 to n',
 	      example: 'run(5) -> 15',
-	      answer_key: 'function(n) { if (isNaN(n)) { return false; } return n * (n + 1)/2 }',
+	      answerKey: 'function(n) { if (isNaN(n)) { return false; } return n * (n + 1)/2 }',
 	      alwaysTest: [0, 1, 10],
 	      randomTest: [2, 3, 4, 5, 6, 7, 8, 9]
 	      
 	    }, {
 	      question: 'Write a function that reverses the word order (but not the words themselves) in a sentence',
 	      example: 'run("How are you?") -> you? are How',
-	      answer_key: 'function(s) { return s.split(" ").reverse().join(" "); }',
+	      answerKey: 'function(s) { return s.split(" ").reverse().join(" "); }',
 	      alwaysTest: ["Hello world!", "JustOneWord", "One Two Three Four Five"],
 	      randomTest: ["Five minus Four = 1"]
 	    }, {
 	      question: 'Given an array of 3 int values, a b c, return their sum. However, if one of the values is 13 then it does not count towards the sum and the immediate value to its right does not count. So for example, if b is 13, then both b and c do not count.',
 	      example: 'run([1, 2, 3]) -> 6, run([1, 2, 13]) -> 3, run([1, 13, 2]) -> 1',
-	      answer_key: 'function(s) { var result = 0; for (var i = 0; i < s.length; i++) { if (s[i] == 13) { i++; } else { result += s[i]; } } return result; }',
+	      answerKey: 'function(s) { var result = 0; for (var i = 0; i < s.length; i++) { if (s[i] == 13) { i++; } else { result += s[i]; } } return result; }',
 	      alwaysTest: [[1, 2, 3], [3, 5, 13], [1, -13, 2], [13, 13, 2], [-13, 13, 2], [13, 13, 13]],
 	      randomTest: [[1, 13, 4], [99, -13, 33], [13, -13, 13]]
 	    }]
@@ -28,7 +28,7 @@ jQuery.hakadoo = ( function($) {
     function _validate(questionId, userCode){
       var question = _questions[questionId]
 				, solution = $.globalEval(userCode)
-				, attempt = $.globalEval(question.answer_key);      
+				, attempt = $.globalEval(question.answerKey);      
 
       // Pass all the required tests
       for(i = 0; i < question.alwaysTest.length; i++){
--- server/sockets.js
+++ server/sockets.js
@@ -39,35 +39,38 @@ exports.startListening = function() {
  * @param (Array) players
  */ 
 function bindBattleLogic(players) {
+  getRandomQuestion(function(error, question) {
+      if (error) this.res.end();
 
-  players.forEach(function(me, i) {
-    var opponent = players[i^1];
+      players.forEach(function(me, i) {
+          var opponent = players[i^1];
 
-    me.emit('ready', {opponent: opponent.hakadoo});
+          me.emit('ready', {opponent: opponent.hakadoo, question: question});
 
-    // When a player enters text, inform the opponent
-    me.on('textEntered', function(data) {
-      var text = data.text;
-      opponent.emit('textUpdate', { text: text });
-    });
+          // When a player enters text, inform the opponent
+          me.on('textEntered', function(data) {
+            var text = data.text;
+            opponent.emit('textUpdate', { text: text });
+          });
 
-    // For the battle actions, simply relay the command from player to player
-    me.on('remove', function() {
-      opponent.emit('remove', {});
-    });
+          // For the battle actions, simply relay the command from player to player
+          me.on('remove', function() {
+            opponent.emit('remove', {});
+          });
 
-    me.on('swap', function() {
-      opponent.emit('swap', {});
-    });
+          me.on('swap', function() {
+            opponent.emit('swap', {});
+          });
 
-    me.on('compile', function(data) {
-      if (data.worked) {
-        opponent.emit('lose', {})
-      }
-    });
+          me.on('compile', function(data) {
+            if (data.worked) {
+              opponent.emit('lose', {})
+            }
+          });
 
-    me.on('peek', function() {
-      opponent.emit('peek', {});
-    });
+          me.on('peek', function() {
+            opponent.emit('peek', {});
+          });
+        });
   });
 }
